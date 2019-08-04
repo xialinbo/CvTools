f2bd5db3d39894e30399ff008d7bc27960e571e2
limbo <xialinbo@hotmail.com>
2019-08-04 19:09:45 +0800

    refactor get_vertices fn

--- src/dl_visualize_output.py
+++ src/dl_visualize_output.py
@@ -7,22 +7,23 @@ import re
 def visualize(image, line):
     width = image.shape[1]
     height = image.shape[0]
-    (x, y, w, h) = get_vertices(line, width, height)
-    top_left = (int(x * width), int(y * height))
-    bottom_right = (int((x + w) * width), int((y + h) * height))
+    (top_left, bottom_right) = get_vertices(line, width, height)
 
     cv.rectangle(image, top_left, bottom_right, (0, 0, 255), 2)
     cv.imshow("foo", image)
     cv.waitKey(1000)
 
-def get_vertices(line, imageWidth, imageHeight):
+def get_vertices(line, image_width, image_height):
     match = re.match(r"^(\d) ([\d\.]+) ([\d\.]+) ([\d\.]+) ([\d\.]+)$", line)
     center_x = float(match.group(2))
     center_y = float(match.group(3))
     width = float(match.group(4))
     height = float(match.group(5))
 
-    return (center_x - width/2, center_y - height/2, width, height)
+    top_left = (int((center_x - width/2) * image_width), int((center_y - height/2) * image_height))
+    bottom_right = (int((center_x + width/2) * image_width), int((center_y + height/2) * image_height))
+
+    return (top_left, bottom_right)
 
 print("processing....")
 
